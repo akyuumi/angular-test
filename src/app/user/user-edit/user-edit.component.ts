f06c8bf65fe4d9206b928ba6714fbdec34cf316f
akyuumi <akyuumi.pg@gmail.com>
2024-03-31 20:18:48 +0900

    フォームを追加

--- src/app/user/user-edit/user-edit.component.ts
+++ src/app/user/user-edit/user-edit.component.ts
@@ -1,12 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+
+import { User } from '../user';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-user-edit',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
+  providers: [UserService],
   templateUrl: './user-edit.component.html',
-  styleUrl: './user-edit.component.css'
+  styleUrl: './user-edit.component.css',
 })
-export class UserEditComponent {
+export class UserEditComponent implements OnInit {
+  user: User = { id: 0, name: '', email: '' };
 
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private service: UserService
+  ) {}
+
+  ngOnInit(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    var user = this.service.getUser(id);
+    if (user) {
+      this.user = user;
+    }
+  }
+
+  onSubmit(form: any): void {
+    let user = {
+      id: form.id,
+      name: form.name,
+      email: form.email,
+    };
+    this.service.setUser(user);
+    this.router.navigate(['/users']);
+  }
 }
--- src/app/user/user-list/user-list.component.ts
+++ src/app/user/user-list/user-list.component.ts
@@ -2,11 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { User } from '../user';
 import { CommonModule } from '@angular/common';
 import { UserService } from '../user.service';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-user-list',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   providers: [UserService],
   templateUrl: './user-list.component.html',
   styleUrls: ['./user-list.component.css'],
